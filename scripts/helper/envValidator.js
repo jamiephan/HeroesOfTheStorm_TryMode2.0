47792ae2df2f5711e797ccb6445fda7a42781999
Jamie Phan <jamie_phan@hotmail.com>
2020-06-06 08:30:10 +0100

    Updated validator due to the .env.example change

--- scripts/helper/envValidator.js
+++ scripts/helper/envValidator.js
@@ -1,13 +1,25 @@
 /* eslint-disable max-len */
 const fs = require('fs');
-require('dotenv').config();
-
+const appRoot = require('app-root-path');
 // eslint-disable-next-line arrow-body-style
 const ErrorGenerator = (key, message) => {
   return new Error(`Configuation File Error: ${key} is not valid. ${message}`);
 };
 
 module.exports.check = () => {
+  // Temporary change to project root dir for "./ to work"
+  const currentWD = process.cwd();
+  process.chdir(appRoot.path);
+
+  if (fs.existsSync(`${appRoot.path}/.env.example`) && !(fs.existsSync(`${appRoot.path}/.env`))) {
+    throw new Error('.env file not found. Did you forget to rename .env.example to .env?');
+  }
+  if (!(fs.existsSync(`${appRoot.path}/.env`))) {
+    throw new Error('.env file not found. Please refer to .env.example in the repo.');
+  }
+  // eslint-disable-next-line global-require
+  require('dotenv').config();
+
   if (!fs.existsSync(process.env.HEROES_OF_THE_STORM_INSTALL_LOCATION)) {
     throw ErrorGenerator(
       'HEROES_OF_THE_STORM_INSTALL_LOCATION',
@@ -39,4 +51,7 @@ module.exports.check = () => {
       `Not a valid Heroes Of The Storm Directory (${process.env.HEROES_OF_THE_STORM_INSTALL_LOCATION})`,
     );
   }
+
+  // Change WD back to the currrent one
+  process.chdir(currentWD);
 };
