8314c934eb5e34ec52ab813fe0c931242046b0ec
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2020-12-05 17:31:28 +0000

    Minor Update to Patch Library tool

--- scripts/patchLibraries.js
+++ scripts/patchLibraries.js
@@ -5,10 +5,7 @@ const fileWalker = require('./helper/fileWalker');
 const removingString = '//_heroes_replace_//';
 
 fileWalker(process.env.TOOLS_UPDATE_LIBRARY_LIBRARIES_LOCATION, (file) => {
-  const fileName = file.split('/')[file.split('/').length - 1];
-  const fileExt = fileName.split('.')[fileName.split('.').length - 1];
-
-  if (fileExt.toLowerCase() === 'galaxy') {
+  if (file.toLowerCase().endsWith('galaxy')) {
     let content = fs.readFileSync(file, { encoding: 'utf8' });
     content = content.replace(new RegExp(removingString, 'gi'), '');
     fs.writeFileSync(file, content, { encoding: 'utf8' });
